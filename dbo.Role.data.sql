2868798ba772db3e7b9c06d2c4212ff8a585a45b
RoxanaParau <roxana.parau@xivic.com>
2015-05-25 17:03:29 +0300

    ver2: adding files

---
+++ dbo.Role.data.sql
@@ -0,0 +1,6 @@
+﻿SET IDENTITY_INSERT [dbo].[Role] ON
+INSERT INTO [dbo].[Role] ([ID], [Name]) VALUES (1, N'Admin')
+INSERT INTO [dbo].[Role] ([ID], [Name]) VALUES (2, N'ContentManager')
+INSERT INTO [dbo].[Role] ([ID], [Name]) VALUES (3, N'Editor')
+INSERT INTO [dbo].[Role] ([ID], [Name]) VALUES (4, N'SimpleUser')
+SET IDENTITY_INSERT [dbo].[Role] OFF
---
+++ dbo.User.data.sql
@@ -0,0 +1,5 @@
+﻿SET IDENTITY_INSERT [dbo].[User] ON
+INSERT INTO [dbo].[User] ([ID], [Username], [Password], [Email], [role_ID]) VALUES (1, N'Test1', N'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3', N'test1@test.coom', 1)
+INSERT INTO [dbo].[User] ([ID], [Username], [Password], [Email], [role_ID]) VALUES (2, N'Test2', N'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3', N'test2@test.coom', 2)
+INSERT INTO [dbo].[User] ([ID], [Username], [Password], [Email], [role_ID]) VALUES (3, N'Test3', N'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3', N'test3@test.coom', 3)
+SET IDENTITY_INSERT [dbo].[User] OFF
